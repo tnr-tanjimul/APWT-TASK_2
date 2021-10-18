550cc8f3ea6dabb10513a185128816da7408fc65
MD Tanjimul Islam <67940292+tnr-tanjimul@users.noreply.github.com>
2021-10-18 14:47:12 +0600

    add title section

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -11,90 +11,90 @@
 <div class="auth-fluid">
     <!--Auth fluid left content -->
     <div class="auth-fluid-form-box">
-                <div class="align-items-center d-flex h-100">
-                    <div class="card-body">
+        <div class="align-items-center d-flex h-100">
+            <div class="card-body">
 
-                        <!-- Logo -->
-                        <div class="auth-brand text-center text-lg-left">
-                            <div class="auth-logo">
-                                <a href="index.html" class="logo logo-dark text-center">
-                                    <span class="logo-lg">
-                                        <img src="../assets/images/logo-dark.png" alt="" height="22">
-                                    </span>
-                                </a>
-            
-                                <a href="index.html" class="logo logo-light text-center">
-                                    <span class="logo-lg">
-                                        <img src="../assets/images/logo-light.png" alt="" height="22">
-                                    </span>
-                                </a>
+                <!-- Logo -->
+                <div class="auth-brand text-center text-lg-left">
+                    <div class="auth-logo">
+                        <a href="index.html" class="logo logo-dark text-center">
+                            <span class="logo-lg">
+                                <img src="../assets/images/logo-dark.png" alt="" height="22">
+                            </span>
+                        </a>
+
+                        <a href="index.html" class="logo logo-light text-center">
+                            <span class="logo-lg">
+                                <img src="../assets/images/logo-light.png" alt="" height="22">
+                            </span>
+                        </a>
+                    </div>
+                </div>
+
+                <!-- title-->
+                <h4 class="mt-0"></h4>
+                <p class="text-muted mb-4">Don't have an account? Create your account, it takes less than a minute</p>
+
+                <!-- form -->
+                <form action="#">
+                    <div class="form-group">
+                        <label for="fullname">Full Name</label>
+                        <input class="form-control" type="text" id="fullname" placeholder="Enter your name" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="emailaddress">Email address</label>
+                        <input class="form-control" type="email" id="emailaddress" required placeholder="Enter your email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <div class="input-group input-group-merge">
+                            <input type="password" id="password" class="form-control" placeholder="Enter your password">
+                            <div class="input-group-append" data-password="false">
+                                <div class="input-group-text">
+                                    <span class="password-eye"></span>
+                                </div>
                             </div>
                         </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="checkbox-signup">
+                            <label class="custom-control-label" for="checkbox-signup">I accept <a href="javascript: void(0);" class="text-dark">Terms and Conditions</a></label>
+                        </div>
+                    </div>
+                    <div class="form-group mb-0 text-center">
+                        <button class="btn btn-primary waves-effect waves-light btn-block" type="submit"> Sign Up </button>
+                    </div>
+                    <!-- social-->
+                    <div class="text-center mt-4">
+                        <p class="text-muted font-16">Sign in with</p>
+                        <ul class="social-list list-inline mt-3 mb-0">
+                            <li class="list-inline-item">
+                                <a href="javascript: void(0);" class="social-list-item border-primary text-primary"><i class="mdi mdi-facebook"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="javascript: void(0);" class="social-list-item border-danger text-danger"><i class="mdi mdi-google"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="javascript: void(0);" class="social-list-item border-info text-info"><i class="mdi mdi-twitter"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="javascript: void(0);" class="social-list-item border-secondary text-secondary"><i class="mdi mdi-github"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </form>
+                <!-- end form-->
 
-                        <!-- title-->
-                        <h4 class="mt-0"></h4>
-                        <p class="text-muted mb-4">Don't have an account? Create your account, it takes less than a minute</p>
+                <!-- Footer-->
+                <footer class="footer footer-alt">
+                    <p class="text-muted">Already have account? <a href="auth-login-2.html" class="text-muted ml-1"><b>Log In</b></a></p>
+                </footer>
 
-                        <!-- form -->
-                        <form action="#">
-                            <div class="form-group">
-                                <label for="fullname">Full Name</label>
-                                <input class="form-control" type="text" id="fullname" placeholder="Enter your name" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="emailaddress">Email address</label>
-                                <input class="form-control" type="email" id="emailaddress" required placeholder="Enter your email">
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <div class="input-group input-group-merge">
-                                    <input type="password" id="password" class="form-control" placeholder="Enter your password">
-                                    <div class="input-group-append" data-password="false">
-                                        <div class="input-group-text">
-                                            <span class="password-eye"></span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" id="checkbox-signup">
-                                    <label class="custom-control-label" for="checkbox-signup">I accept <a href="javascript: void(0);" class="text-dark">Terms and Conditions</a></label>
-                                </div>
-                            </div>
-                            <div class="form-group mb-0 text-center">
-                                <button class="btn btn-primary waves-effect waves-light btn-block" type="submit"> Sign Up </button>
-                            </div>
-                            <!-- social-->
-                            <div class="text-center mt-4">
-                                <p class="text-muted font-16">Sign in with</p>
-                                <ul class="social-list list-inline mt-3 mb-0">
-                                    <li class="list-inline-item">
-                                        <a href="javascript: void(0);" class="social-list-item border-primary text-primary"><i class="mdi mdi-facebook"></i></a>
-                                    </li>
-                                    <li class="list-inline-item">
-                                        <a href="javascript: void(0);" class="social-list-item border-danger text-danger"><i class="mdi mdi-google"></i></a>
-                                    </li>
-                                    <li class="list-inline-item">
-                                        <a href="javascript: void(0);" class="social-list-item border-info text-info"><i class="mdi mdi-twitter"></i></a>
-                                    </li>
-                                    <li class="list-inline-item">
-                                        <a href="javascript: void(0);" class="social-list-item border-secondary text-secondary"><i class="mdi mdi-github"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </form>
-                        <!-- end form-->
-
-                        <!-- Footer-->
-                        <footer class="footer footer-alt">
-                            <p class="text-muted">Already have account? <a href="auth-login-2.html" class="text-muted ml-1"><b>Log In</b></a></p>
-                        </footer>
-
-                    </div> <!-- end .card-body -->
-                </div> <!-- end .align-items-center.d-flex.h-100-->
-            </div>
-            <!-- end auth-fluid-form-box-->
+            </div> <!-- end .card-body -->
+        </div> <!-- end .align-items-center.d-flex.h-100-->
+    </div>
+    <!-- end auth-fluid-form-box-->
 
     <!-- Auth fluid right content -->
     <div class="auth-fluid-right text-center">
